4244da1edec5a58752ce883e2300bbf9ebdb455d
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-07-18 15:05:40 +0300

    Add files via upload

---
+++ react01/react-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import product from './assets/product'
+import './App.css'
+
+function App() {
+  return  (
+  
+    <product/>
+
+     )
+}
+
+export default App;
---
+++ react01/react-project/src/assets/product.jsx
@@ -0,0 +1,13 @@
+
+
+function product () 
+{
+
+    return (
+        <div>
+            <h1>I  am a product  component</h1>
+        </div>
+    )
+}
+
+export default product;
